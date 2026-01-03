50774818ddf8f9c770025a83e76db413f8659676
bobbill45 <jakesing92@gmail.com>
2026-01-02 19:25:43 -1000

    Update survey.py

--- survey.py
+++ survey.py
@@ -22,11 +22,11 @@ st.title("Business spreadsheet cleaning survey")
 st.image(buffer, caption="Scan me please")
 st.write("---")
 
-q1 = st.selectbox ("What type of spreadsheet do you work with the most?", ["Customers", "Inventory","Vendors","Timecards", "Sale reports", "others"])
-q2 = st.selectbox ("How often do you receive messy or inconsistent speadsheets?", ["Daily", "Weekly", "monthly", "Rarely", "Constantly"])
-q3 = st.selectbox("What are the problems do you see the most often?", ["Duplicates", "Wrong unit messurement","Missspellings", "Missing columns", "Wrong formats","Hidden Security theats"])
-q4 = st.selectbox("What system do you import data into?", ["Quickbooks", "Xero", "POS systems", "Customer software","Not sure"])
-q5 = st.selectbox("Would you use a tool that cleans and secures your spreadsheets automatically?", ["Yes", "Maybe", "Only if cheap", "No"])
+q1 = st.radio("What type of spreadsheet do you work with the most?", ["Customers", "Inventory","Vendors","Timecards", "Sale reports", "others"])
+q2 = st.radio("How often do you receive messy or inconsistent speadsheets?", ["Daily", "Weekly", "monthly", "Rarely", "Constantly"])
+q3 = st.radio("What are the problems do you see the most often?", ["Duplicates", "Wrong unit messurement","Missspellings", "Missing columns", "Wrong formats","Hidden Security theats"])
+q4 = st.radio("What system do you import data into?", ["Quickbooks", "Xero", "POS systems", "Customer software","Not sure"])
+q5 = st.radio("Would you use a tool that cleans and secures your spreadsheets automatically?", ["Yes", "Maybe", "Only if cheap", "No"])
 
     
 
